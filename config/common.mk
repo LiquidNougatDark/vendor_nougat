b7d32c2aa4ecab4e7bc48decfecbe5ead9f0dc19
liquid0624 <rjbirnesser@gmail.com>
2015-10-16 20:07:42 -0400

    Remove unwanted build prop overrides

--- config/common.mk
+++ config/common.mk
@@ -11,15 +11,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
-    ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false \
-    drm.service.enabled=true \
-    net.tethering.noprovisioning=true \
     persist.sys.dun.override=0 \
-    ro.setupwizard.network_required=false \
-    ro.setupwizard.gservices_delay=-1 \
-    ro.build.selinux=1 \
-    ro.adb.secure=1
+    ro.build.selinux=1
 
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += \
