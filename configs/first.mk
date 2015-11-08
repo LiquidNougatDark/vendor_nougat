0fb9c8b63f2dd8c90bee3ab4d916795643f90e84
Chet Kener <Cl3Kener@gmail.com>
2015-11-08 03:10:30 -0500

    UBER build property overrides
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- configs/first.mk
+++ configs/first.mk
@@ -8,11 +8,28 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
+
+# UBER property overides
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.supplicant_scan_interval=180 \
+    windowsmgr.max_events_per_sec=150 \
+    persist.service.lgospd.enable=0 \
+    persist.service.pcsync.enable=0 \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false \
     ro.setupwizard.network_required=false \
+    ro.setupwizard.gservices_delay=-1 \
     net.tethering.noprovisioning=true \
     persist.sys.dun.override=0 \
-    ro.debuggable=1 \
-    ro.adb.secure=0
+    ro.sys.fw.bg_apps_limit=20 \
+    debug.performance.tuning=1 \
+    ro.ril.power_collapse=1 \
+    pm.sleep.mode=1 \
+    ro.adb.secure=1
 
 # Enable SIP+VoIP
 PRODUCT_COPY_FILES += \
