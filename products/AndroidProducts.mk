d52798f481f1e61aa1e222952fa44844700772f9
liquid0624 <rjbirnesser@gmail.com>
2015-12-03 13:51:06 -0500

    Add mallow hammerhead product and config

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,4 +1,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/mallow_angler.mk \
+    $(LOCAL_DIR)/mallow_hammerhead.mk \
     $(LOCAL_DIR)/mallow_shamu.mk
-
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
 add_lunch_combo mallow_angler-userdebug
+add_lunch_combo mallow_hammerhead-userdebug
 add_lunch_combo mallow_shamu-userdebug
 
