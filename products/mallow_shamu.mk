965285ec884074e796b76d73d05dd57cd63f54b8
liquid0624 <rjbirnesser@gmail.com>
2016-04-18 19:23:34 -0400

    shamu: update fingerprint to MOB30D

--- products/mallow_shamu.mk
+++ products/mallow_shamu.mk
@@ -28,5 +28,5 @@ PRODUCT_MANUFACTURER := Motorola
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT="google/shamu/shamu:6.0.1/MMB29V/2554798:user/release-keys"
-    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MMB29V 2554798 release-keys"
+    BUILD_FINGERPRINT="google/shamu/shamu:6.0.1/MOB30D/2704746:user/release-keys"
+    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30D 2704746 release-keys"
