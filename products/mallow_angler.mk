24135292e307ad9f5cdf6e5775beb13443f6c032
liquid0624 <rjbirnesser@gmail.com>
2016-05-21 21:49:12 -0400

    angler: use gcc arm64 version 6.0 now

--- products/mallow_angler.mk
+++ products/mallow_angler.mk
@@ -38,7 +38,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 endif
 
 # Inline kernel building
-TARGET_GCC_VERSION_ARM64 := 7.0
+TARGET_GCC_VERSION_ARM64 := 6.0
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
 TARGET_KERNEL_CONFIG := saber_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
