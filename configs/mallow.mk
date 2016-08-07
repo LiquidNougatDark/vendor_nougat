8d79160fa8c4d469ed92c6b2be966ca7998fa96b
liquid0624 <rjbirnesser@gmail.com>
2016-08-07 10:39:16 -0400

    vendor: run system makefile before mallow

--- configs/mallow.mk
+++ configs/mallow.mk
@@ -3,10 +3,7 @@ export USE_CCACHE := 1
 export USE_SYSTEM_CCACHE := 1
 export ANDROID_COMPILE_WITH_JACK := true
 
-# Target device
-export TARGET_DEVICE := angler
-
-# Optimizes build
+# Optimize build
 export CLANG_O3 := true
 export GRAPHITE_OPTS := true
 ifeq ($(TARGET_DEVICE),shamu)
@@ -20,14 +17,10 @@ export ENABLE_SANITIZE := true
 export STRICT_ALIASING := true
 export USE_PIPE := true
 
-# Toolchain versions
-export TARGET_NDK_GCC_VERSION := 4.9
-export TARGET_GCC_VERSION_EXP := 4.9
+# Kernel toolchain
 ifeq ($(TARGET_DEVICE),shamu)
-export TARGET_GCC_VERSION := 5.4
 export TARGET_GCC_VERSION_ARM := 7.0
 else
-export TARGET_GCC_VERSION := 6.1
 export TARGET_GCC_VERSION_ARM64 := 7.0
 endif
 
--- vendor.mk
+++ vendor.mk
@@ -1,2 +1,6 @@
 # Export rom name
 export VENDOR := mallow
+
+# Target device name
+export TARGET_DEVICE := angler
+
