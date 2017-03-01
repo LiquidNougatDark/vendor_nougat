ebeac89a8c94b54b64ac583985f65c99d6e36fa2
liquid0624 <rjbirnesser@gmail.com>
2017-02-28 19:00:41 -0500

    vendor: update uber property overrides
    Thanks chad :)

--- config/common.mk
+++ config/common.mk
@@ -4,34 +4,32 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.setupwizard.enterprise_mode=1
+    ro.setupwizard.rotation_locked=true
 
 # UBER property overides
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.performance.tuning=1 \
+    ro.sys.fw.bg_apps_limit=20 \
     wifi.supplicant_scan_interval=180 \
     windowsmgr.max_events_per_sec=150 \
+    debug.performance.tuning=1 \
+    ro.ril.power_collapse=1 \
     persist.service.lgospd.enable=0 \
     persist.service.pcsync.enable=0 \
-    persist.sys.dun.override=0 \
     ro.facelock.black_timeout=400 \
     ro.facelock.det_timeout=1500 \
     ro.facelock.rec_timeout=2500 \
     ro.facelock.lively_timeout=2500 \
     ro.facelock.est_max_time=600 \
     ro.facelock.use_intro_anim=false \
-    ro.ril.power_collapse=1 \
     ro.setupwizard.network_required=false \
     ro.setupwizard.gservices_delay=-1 \
-    ro.sys.fw.bg_apps_limit=20 \
-    ro.substratum.verified=true \
     net.tethering.noprovisioning=true \
+    persist.sys.dun.override=0 \
+    ro.substratum.verified=true \
+    ro.opa.eligible_device=true \
     ro.adb.secure=1
-
-# Google assistant
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    ro.opa.eligible_device=true
 
 # APN config
 PRODUCT_COPY_FILES += \
@@ -63,6 +61,10 @@ PRODUCT_PACKAGES += \
     masquerade \
     QuickSearchBox
 
+# Facelock issues
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full
+
 # Exclude SystemUI tests
 EXCLUDE_SYSTEMUI_TESTS := true
 
@@ -71,4 +73,3 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/nougat/overlay/common
 
 # Inherit build prop overrides
 -include vendor/nougat/config/common_version.mk
-
